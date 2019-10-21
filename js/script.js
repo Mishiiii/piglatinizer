588ae1dc37a887cad9d140a954e48a70902752d4
Mishiiii <sayma.mishi@uasdc.org>
2019-10-21 22:07:44 +0000

    10/21/2019 piglatinizer-javascript

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+function pig(Str)
+{
+    return str;
+}
+
+
+$("button").click(() => {
+    let message = $("input").val();
+    $("body").append(message);
+});
+
+function pig(str)
 
 
 
-  
-  
-  
-  
+
+
+
+
+
+
+
 });
